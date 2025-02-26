02af82ab8a140d77a344f9b0cfa71a97d2352e45
jantar <mikikubs@gmail.com>
2025-02-26 18:10:58 +0100

    feat: bullets pushing away on life end

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -15,6 +15,19 @@ public class Bullet : PixelatedRigidbody
         DelayedFadeOutAsync().Forget();
     }
 
+    private void OnDestroy()
+    {
+        var results = new Collider2D[5];
+        Physics2D.OverlapCircle(transform.position, 10f, new ContactFilter2D(), results);
+
+        foreach (var result in results)
+        {
+            Debug.Log(result);
+            result?.attachedRigidbody.AddForce((result.transform.position - transform.position) * 10,
+                ForceMode2D.Impulse);
+        }
+    }
+
     private async UniTaskVoid DelayedFadeOutAsync()
     {
         try
