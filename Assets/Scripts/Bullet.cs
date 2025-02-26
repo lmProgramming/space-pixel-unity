d1fc7d6f7cc10b6c01baa6f2f2017ab97cca8e3c
jantar <mikikubs@gmail.com>
2025-02-26 18:12:15 +0100

    feat: bullets pushing away on life end

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 
 public class Bullet : PixelatedRigidbody
 {
+    private const float PushAwayRadius = 10f;
+    private const float PushAwayStrength = 10f;
+
     [SerializeField] private float fadeOutTime = 2f;
     [SerializeField] private float lifeTime = 2f;
 
@@ -18,14 +21,11 @@ public class Bullet : PixelatedRigidbody
     private void OnDestroy()
     {
         var results = new Collider2D[5];
-        Physics2D.OverlapCircle(transform.position, 10f, new ContactFilter2D(), results);
+        Physics2D.OverlapCircle(transform.position, PushAwayRadius, new ContactFilter2D(), results);
 
         foreach (var result in results)
-        {
-            Debug.Log(result);
-            result?.attachedRigidbody.AddForce((result.transform.position - transform.position) * 10,
+            result?.attachedRigidbody.AddForce((result.transform.position - transform.position) * PushAwayStrength,
                 ForceMode2D.Impulse);
-        }
     }
 
     private async UniTaskVoid DelayedFadeOutAsync()
