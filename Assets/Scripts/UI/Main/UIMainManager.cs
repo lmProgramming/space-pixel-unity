93ea8db24a0d9ae16f5f0915e5403e2d2f060ead
jantar <mikikubs@gmail.com>
2025-03-28 23:04:01 +0100

    feat: speed UI

---
+++ Assets/Scripts/UI/Main/UIMainManager.cs
@@ -0,0 +1,17 @@
+using TMPro;
+using UnityEngine;
+
+namespace UI.Main
+{
+    public class UIMainManager : MonoBehaviour
+    {
+        public Ship.Ship playerShip;
+        public TextMeshProUGUI speedText;
+
+        private void Update()
+        {
+            speedText.SetText(
+                playerShip.CommandModule.PixelatedRigidbody.Rigidbody.linearVelocity.magnitude.ToString("F1"));
+        }
+    }
+}
